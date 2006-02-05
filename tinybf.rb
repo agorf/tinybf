f5f379e2754a6346e2e6e373e8d93ff81a5bde41
Aggelos Orfanakos <agorf@agorf.gr>
2006-02-05 02:29:47 +0000

    Changed description. Replaced loop with while, fixing a bug that occured if no input was given. Dropped to 403 bytes.

--- tinybf.rb
+++ tinybf.rb
@@ -1,6 +1,6 @@
 #!/usr/bin/env ruby
 
-# = tinybf.rb - 417-byte Brainfuck interpreter in Ruby
+# = tinybf.rb - tiny Brainfuck interpreter in Ruby
 #
 # URL::       http://agorf.void.gr/code/tinybf.rb
 # Author::    Aggelos Orfanakos (http://agorf.void.gr)
@@ -18,7 +18,7 @@
 
 c,i=STDIN.read.split'!',2;c=c.scan(/[-+<>\[\],.]/).join;i||=[];b,h,z,*a=0,0,0,0;
 j,s={},[];c.length.times{|n|case c[n].chr;when'[':s<<n;when']':j[n]=s.pop;j[j[n]
-]=n;end};loop{case c[b].chr;when'+':a[z]=(a[z]+1)%256;when'-':a[z]=(a[z]-1)%256;
-when'>':z+=1;a<<0if !a[z];when'<':z-=z==0?0:1;when'.':putc a[z].chr;when'[':b=j[
-b]if a[z]==0;when',':if i[h]:a[z]=i[h];h+=1;end;when']':b=j[b]if a[z]!=0;end;b+=
-1;break if !c[b]}
+]=n;end};while c[b]:case c[b];when 43:a[z]=(a[z]+1)%256;when 62:z+=1;a<<0if !a[z
+];when 91:b=j[b]if a[z]==0;when 46:putc a[z];when 93:b=j[b]if a[z]!=0;when 45:a[
+z]=(a[z]-1)%256;when 44:if i[h]:a[z]=i[h];h+=1;end;when 60:z-=z==0?0:1;end;b+=1;
+end
