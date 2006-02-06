651914159a532e435f00e75611356c41476d0642
Aggelos Orfanakos <agorf@agorf.gr>
2006-02-06 15:31:53 +0000

    Fixed a bug that occured when the program was started and ^D was given as input.
    Brought the size down to 397 bytes mainly by using && instead of if's.

--- tinybf.rb
+++ tinybf.rb
@@ -16,9 +16,9 @@
 # s tack
 #++
 
-c,i=STDIN.read.split'!',2;c=c.scan(/[-+<>\[\],.]/).join;i||=[];b,h,z,*a=0,0,0,0;
-j,s={},[];c.length.times{|n|case c[n].chr;when'[':s<<n;when']':j[n]=s.pop;j[j[n]
-]=n;end};while c[b]:case c[b];when 43:a[z]=(a[z]+1)%256;when 62:z+=1;a<<0if !a[z
-];when 91:b=j[b]if a[z]==0;when 46:putc a[z];when 93:b=j[b]if a[z]!=0;when 45:a[
-z]=(a[z]-1)%256;when 44:if i[h]:a[z]=i[h];h+=1;end;when 60:z-=z==0?0:1;end;b+=1;
+c,i=STDIN.read.split'!',2;c||='';c=c.scan(/[-+<>\[\],.]/).join;i||=[];b,h,z,*a=
+0,0,0,0;j,s={},[];c.length.times{|n|case c[n];when 91:s<<n;when 93:j[n]=s.pop;j[
+j[n]]=n;end};while c[b]:case c[b];when 43:a[z]=(a[z]+1)%256;when 46:putc a[z];
+when 91:a[z]==0&&b=j[b];when 62:!a[z+=1]&&a<<0;when 93:a[z]!=0&&b=j[b];when 45:
+a[z]=(a[z]-1)%256;when 44:i[h]&&(a[z]=i[h];h+=1);when 60:z-=z==0?0:1;end;b+=1;
 end
