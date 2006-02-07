499675ed78b15c07006acbc8353934e614917a90
Aggelos Orfanakos <agorf@agorf.gr>
2006-02-07 15:52:13 +0000

    Split tests into separate methods created dynamically.

--- test/test_tinybf.rb
+++ test/test_tinybf.rb
@@ -7,16 +7,15 @@ require 'test/unit'
 class TestTinyBF < Test::Unit::TestCase
   TESTS = ['30000', 'io', 'numwarp', 'obscure', 'rot13', 'triangle']
 
-  def test_tests
-    TESTS.each do |title|
-      code = File.open(File.join($:[0], 'test', "#{title}.b")) {|f| f.read }
-      result = File.open(File.join($:[0], 'test', "#{title}.txt")) {|f| f.read }
-
-      IO.popen('ruby ' + File.join($:[0], 'tinybf.rb'), 'r+') do |f|
-        f.write(code)
-        f.close_write
-        assert_equal(result, f.read)
-      end
+  TESTS.each do |title|
+    expected = File.open(File.join($:[0], 'test', "#{title}.txt")) {|f| f.read }
+    code = File.open(File.join($:[0], 'test', "#{title}.b")) {|f| f.read }
+    actual = IO.popen('ruby ' + File.join($:[0], 'tinybf.rb'), 'r+') do |f|
+      f.write(code)
+      f.close_write
+      f.read
     end
+
+    define_method("test_#{title}") { assert_equal(expected, actual) }
   end
 end
