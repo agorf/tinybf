7986a8f436e17ed75d227b780c0d586a3fc6c0a0
Aggelos Orfanakos <agorf@agorf.gr>
2010-01-18 10:33:33 +0200

    Remove header

--- tinybf.rb
+++ tinybf.rb
@@ -1,20 +1,10 @@
 #!/usr/bin/env ruby
 
-# = tinybf.rb - tiny Brainfuck interpreter in Ruby
-#
-# URL::       http://agorf.void.gr/code/tinybf.rb
-# Author::    Aggelos Orfanakos (http://agorf.void.gr)
-# Copyright:: 2006, Aggelos Orfanakos. All rights reserved.
-# License::   MIT[http://agorf.void.gr/code/LICENSE]
-#
-# $Id$
-#--
 # c ode  (b pointer)
 # i nput (h pointer)
 # a rray (z pointer)
 # j ump
 # s tack
-#++
 
 c,i=STDIN.read.split'!',2;(c||='').scan(/[-+<>\[\],.]/)*'';i||=[];b,h,z,*a=0,0,
 0,0;j,s={},[];c.size.times{|n|case c[n];when 91:s<<n;when 93:j[n]=s.pop;j[j[n]]=
