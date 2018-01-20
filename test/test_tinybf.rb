463585dbebeec04e3b26aefce4a5bd8536333e8a
Angelos Orfanakos <me@agorf.gr>
2018-01-20 17:45:25 +0200

    Improve test-running script

--- test/test_tinybf.rb
+++ test/test_tinybf.rb
@@ -1,20 +1,21 @@
 #!/usr/bin/env ruby
 
-$:.unshift File.join(File.dirname(__FILE__), '..') 
-
 require 'test/unit'
 
 class TestTinyBF < Test::Unit::TestCase
-  FILES = Dir.glob(File.join('test', '*.b'))
-
-  FILES.each {|fn|
+  Dir.glob('test/*.b').each do |fn|
     method_name = 'test_' + fn.sub(/\.b$/, '')
-    expected = File.read(fn.sub(/\.b$/, '.out'))
-    actual = open('|ruby tinybf.rb', 'r+') {|f|
-      f.write(File.read(fn))
-      f.close_write
-      f.read
-    }
-    define_method(method_name) { assert_equal(expected, actual) }
-  }
+
+    define_method(method_name) do
+      expected = File.read(fn.sub(/\.b$/, '.out'))
+
+      actual = IO.popen('ruby tinybf.rb', 'r+') do |f|
+        f.write(File.read(fn))
+        f.close_write
+        f.read
+      end
+
+      assert_equal(expected, actual)
+    end
+  end
 end
