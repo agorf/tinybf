46c122136bcd31299bc0b1115254a9ba0b70cf9c
Angelos Orfanakos <me@agorf.gr>
2018-01-20 13:06:52 +0200

    Rewrite for Ruby 2.5.0 compatibility
    Size increased by 4 bytes to 386 bytes.

--- size.rb
+++ size.rb
@@ -1,4 +1,3 @@
 #!/usr/bin/env ruby
 
-puts File.read('tinybf.rb').strip.reverse.split("\n\n", 2)[0].reverse.
-  split("\n").join.size.to_s + ' bytes'
+puts File.read('tinybf.rb').strip.split("\n\n").last.size.to_s + ' bytes'
--- tinybf.rb
+++ tinybf.rb
@@ -6,8 +6,36 @@
 # j ump
 # s tack
 
-c,i=STDIN.read.split'!',2;(c||='').scan(/[-+<>\[\],.]/)*'';i||=[];*a=b=h=z=0;
-j,s={},[];c.size.times{|n|case c[n];when ?[:s<<n;when ?]:j[n]=s.pop;j[j[n]]=n;
-end};while c[b]:case c[b];when ?+:a[z]=(a[z]+1)%256;when ?-:a[z]=(a[z]-1)%256;
-when ?,:i[h]&&(a[z]=i[h];h+=1);when ?.:putc a[z];when ?>:!a[z+=1]&&a<<0;when ?<:
-z-=z>0?1:0;when ?[:a[z]>0||b=j[b];when ?]:a[z]>0&&b=j[b];end;b+=1;end
+c,i=STDIN.read.split'!',2
+(c||='').scan(/[-+<>\[\],.]/)*''
+i||=[]
+*a=b=h=z=0
+j,s={},[]
+c.size.times{|n|case c[n]
+when ?[
+s<<n
+when ?]
+j[n]=s.pop
+j[j[n]]=n
+end}
+while c[b]
+case c[b]
+when ?+
+a[z]=(a[z]+1)%256
+when ?-
+a[z]=(a[z]-1)%256
+when ?,
+i[h]&&(a[z]=i[h].ord;h+=1)
+when ?.
+putc a[z]
+when ?>
+!a[z+=1]&&a<<0
+when ?<
+z-=z>0?1:0
+when ?[
+a[z]>0||b=j[b]
+when ?]
+a[z]>0&&b=j[b]
+end
+b+=1
+end
